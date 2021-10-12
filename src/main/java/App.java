ba1f35b73f93cdd4574d289fc3016dd4b12d1114
nathanstephenson <nathans261@gmail.com>
2021-10-12 16:19:41 +0100

    007: filled out main function to actually run the program via the controller

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,22 @@
+import controller.DVDLibraryController;
+
+import dao.DVDLibraryDaoException;
+import dao.DVDLibraryDaoImpl;
 import dto.DVD;
+import ui.DVDLibraryView;
+import ui.UserIO;
+import ui.UserIOConsoleImpl;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DVDLibraryDaoException {
+        UserIO io = new UserIOConsoleImpl();
+        DVDLibraryView view = new DVDLibraryView(io);
+        DVDLibraryController controller = new DVDLibraryController(new DVDLibraryDaoImpl(), view);
 
+        try {
+            controller.run();
+        } catch(Exception e){
+            throw new DVDLibraryDaoException("Error: ", e);
+        }
     }
 }
